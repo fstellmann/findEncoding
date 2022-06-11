aff98bbc76cc748594a36440a2d4ba625d3decf2
Florian Stellmann <f.stellmann@nasdo.de>
2022-06-11 14:07:13 +0200

    Initial Commit

---
+++ findEncoding/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace findEncoding
+{
+    class Program
+    {
+        private static string path;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var ofd = new OpenFileDialog();
+
+            ofd.ShowDialog();
+            path = ofd.FileName;
+            try
+            {
+                Encoding encoding = path.GetEncoding();
+                MessageBox.Show($"Detected Encoding {encoding.EncodingName} for file {path}");
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+    }
+}
